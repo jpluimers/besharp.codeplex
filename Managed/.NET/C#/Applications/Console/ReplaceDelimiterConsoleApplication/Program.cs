7b87cf96b96d3b083603a244024e918244856ad6
jeroenp_cp <SND\jeroenp_cp>
2012-04-28 13:12:12 +0000

    XCOM file transfer inserted the correct ; delimiter, but manual file transfer made is some high-ASCII (wrong therm, is actually a high some-codepage) character. This quick fix allows you replace any character in a file with another character.

---
+++ Managed/.NET/C#/Applications/Console/ReplaceDelimiterConsoleApplication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReplaceDelimiterConsoleApplication
+{
+    class Program
+    {
+        static void Logic(string sourcedelimiter, string targetdelimiter, string sourcefile, string targetfile)
+        {
+            int codePage = 1252;
+            Encoding encoding = Encoding.GetEncoding(codePage);
+            string[] sourceLines = File.ReadAllLines(sourcefile, encoding);
+            List<string> targetLines = new List<string>();
+            foreach (string sourceLine in sourceLines)
+            {
+                string targetLine = sourceLine.Replace(sourcedelimiter, targetdelimiter);
+                targetLines.Add(targetLine);
+            }
+            File.WriteAllLines(targetfile, targetLines, encoding);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Use 4 arguments: sourcedelimiter targetdelimiter sourcefile targetfile");
+                Console.WriteLine("this will replace all occurances of sourcedelimiter in sourcefile with targetdelimiter in targetfile");
+            }
+            else
+            {
+                Logic(args[0], args[1], args[2], args[3]);
+            }
+        }
+    }
+}
