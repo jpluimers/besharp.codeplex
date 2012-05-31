55b65fac44d98e4731d95f2aaf75e07a313f2271
jeroenp_cp <SND\jeroenp_cp>
2012-05-31 21:41:28 +0000

    SqlConnectionStringBuilderKeysConsoleApplication for http://wiert.me/2012/11/07/netc-sqlclient-connectionstring-keys

---
+++ Managed/.NET/C#/Applications/Console/SqlConnectionStringBuilderKeysConsoleApplication/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace SqlConnectionStringBuilderKeysConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // a few examples of ConnectionStrings from http://connectionstrings.com/sql-server-2008#p1 and their shortened forms.
+
+            showConnectionStringAndShortestConnectionString("Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;");
+            showConnectionStringAndShortestConnectionString("Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;");
+            showConnectionStringAndShortestConnectionString(@"Server=myServerName\theInstanceName;Database=myDataBase;Trusted_Connection=True;");
+            showConnectionStringAndShortestConnectionString(@"Data Source=myServerAddress;Initial Catalog=myDataBase;Integrated Security=SSPI;User ID=myDomain\myUsername;Password=myPassword;");
+            showConnectionStringAndShortestConnectionString("Data Source=190.190.200.100,1433;Network Library=DBMSSOCN;Initial Catalog=myDataBase;User ID=myUsername;Password=myPassword;");
+            showConnectionStringAndShortestConnectionString("Server=myServerAddress;Database=myDataBase;Trusted_Connection=True; MultipleActiveResultSets=true;");
+            showConnectionStringAndShortestConnectionString(@"Server=.\SQLExpress;AttachDbFilename=c:\asd\qwe\mydbfile.mdf;Database=dbname; Trusted_Connection=Yes;");
+            showConnectionStringAndShortestConnectionString(@"Server=.\SQLExpress;AttachDbFilename=|DataDirectory|mydbfile.mdf; Database=dbname;Trusted_Connection=Yes;");
+            showConnectionStringAndShortestConnectionString(@"Data Source=.\SQLExpress;Integrated Security=true; AttachDbFilename=|DataDirectory|\mydb.mdf;User Instance=true;");
+            showConnectionStringAndShortestConnectionString("Data Source=myServerAddress;Failover Partner=myMirrorServerAddress;Initial Catalog=myDataBase;Integrated Security=True;");
+            showConnectionStringAndShortestConnectionString("Server=myServerAddress;Database=myDataBase;Integrated Security=True;Asynchronous Processing=True;");
+
+            // the HTML table of equivalent keys
+
+            IEnumerable<string> keys = SqlConnectionStringBuilderHelper.Keys;
+            IEnumerable<string> orderedKeys = keys.OrderBy(s => s); // http://stackoverflow.com/a/3630693/29290
+
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Key");
+            dataTable.Columns.Add("ShortesEquivalentKey");
+            dataTable.Columns.Add("EquivalentKeys");
+
+            foreach (string key in orderedKeys)
+            {
+                string shortestEquivalentKey = SqlConnectionStringBuilderHelper.ShortestEquivalentKey(key);
+                List<string> equivalentKeys = SqlConnectionStringBuilderHelper.EquivalentKeys(key);
+
+                string equivalentKeysCSV = AsCSV(equivalentKeys);
+
+                dataTable.Rows.Add(key, shortestEquivalentKey, equivalentKeysCSV);
+            }
+
+            string htmlTable = AsHtmlTable(dataTable);
+            Console.WriteLine(htmlTable);
+            Console.ReadLine();
+        }
+
+        private static void showConnectionStringAndShortestConnectionString(string connectionString)
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
+            connectionString = connectionStringBuilder.ConnectionString;
+            string shortestConnectionString = SqlConnectionStringBuilderHelper.ShortestConnectionString(connectionStringBuilder);
+
+            Console.WriteLine(connectionString);
+            Console.WriteLine(shortestConnectionString);
+            Console.WriteLine();
+        }
+
+        // http://stackoverflow.com/a/799454/29290
+        static string AsCSV(IEnumerable<string> strings)
+        {
+            string[] stringsArray = strings.ToArray();
+
+            string joined = string.Join(",", stringsArray);
+
+            return joined;
+        }
+
+        // http://stackoverflow.com/a/1018811/29290
+        static string AsHtmlTable(DataTable dataTable)
+        {
+            // HtmlTextWriter and DataGrid are in System.Web.dll which is not part of the .NET Framework 4 Client Profile; you need the full .NET Framework 4 profile for this
+            DataGrid dataGrid = new DataGrid();
+            dataGrid.DataSource = dataTable;
+            dataGrid.DataBind();
+
+            StringWriter stringWriter = new StringWriter();
+            HtmlTextWriter htmlTextWriter = new HtmlTextWriter(stringWriter);
+
+            dataGrid.RenderControl(htmlTextWriter);
+
+            string result = stringWriter.ToString();
+            return result;
+        }
+
+    }
+}
---
+++ Managed/.NET/C#/Applications/Console/SqlConnectionStringBuilderKeysConsoleApplication/SqlConnectionStringBuilderHelper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Text;
+using System.Globalization;
+
+namespace SqlConnectionStringBuilderKeysConsoleApplication
+{
+    public class SqlConnectionStringBuilderHelper
+    {
+
+        public static IEnumerable<string> Keys
+        {
+            get
+            {
+                SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+                ICollection keys = connectionStringBuilder.Keys;
+                // Reflection shows that SqlConnectionStringBuilder actually returns System.Data.Common.ReadOnlyCollection<string> which is an internal type.
+                // but it implements IEnumerable<string>
+                return (IEnumerable<string>)keys; 
+            }
+        }
+
+        public static List<string> EquivalentKeys(string key)
+        {
+            object keywordsObject = GetStaticPrivateProperty<SqlConnectionStringBuilder>("_keywords");
+            // keywordsObject is actually Dictionary<string, Keywords>, but KeyWords is a private enum inside the SqlClient namespace.
+            IDictionary keywords = (IDictionary)keywordsObject;
+
+            object value = keywords[key];
+
+            List<string> result = new List<string>();
+
+            foreach (DictionaryEntry keyword in keywords)
+            {
+                if (value.Equals(keyword.Value))
+                    result.Add(keyword.Key.ToString());
+            }
+
+            return result;
+        }
+
+        public static string ShortestEquivalentKey(string key)
+        {
+            List<string> equivalentKeys = EquivalentKeys(key);
+
+            string result = key;
+
+            foreach (string equivalentKey in equivalentKeys)
+            {
+                if (equivalentKey.Length < result.Length)
+                    result = equivalentKey;
+            }
+
+            return result;
+        }
+
+        // mimicked after the getter from SqlConnectionStringBuilder.ConnectionString
+        public static string ShortestConnectionString(SqlConnectionStringBuilder connectionStringBuilder)
+        {
+            string result;
+            StringBuilder builder = new StringBuilder();
+            foreach (string key in connectionStringBuilder.Keys)
+            {
+                object valueObject;
+                if (connectionStringBuilder.ShouldSerialize(key) && connectionStringBuilder.TryGetValue(key, out valueObject))
+                {
+                    string value = (valueObject != null) ? Convert.ToString(valueObject, CultureInfo.InvariantCulture) : null;
+                    string shortKey = ShortestEquivalentKey(key);
+                    bool UseOdbcRules = false;
+                    SqlConnectionStringBuilder.AppendKeyValuePair(builder, shortKey, value, UseOdbcRules);
+                }
+            }
+            result = builder.ToString();
+            return result;
+        }
+
+        // http://stackoverflow.com/a/628687/29290
+        protected static object GetStaticPrivateProperty<T>(string name)
+        {
+            Type type = typeof(T);
+            FieldInfo info = type.GetField(name, BindingFlags.NonPublic | BindingFlags.Static);
+            object result = info.GetValue(null);
+            return result;
+        }
+
+    }
+}
