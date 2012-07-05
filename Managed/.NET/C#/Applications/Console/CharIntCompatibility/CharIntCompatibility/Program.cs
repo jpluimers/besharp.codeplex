6b767a29d5bb2cfe686b0165cd7cb761a3c70c32
jeroenp_cp <SND\jeroenp_cp>
2012-07-05 09:44:12 +0000

    CharIntCompatibility

---
+++ Managed/.NET/C#/Applications/Console/CharIntCompatibility/CharIntCompatibility/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace CharIntCompatibilityCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("asterisk");
+            char asterisk = '*';
+            //writeLineSbyte(asterisk); // no implicit conversion
+            //writeLineByte(asterisk); // no implicit conversion
+            //writeLineShort(asterisk); // no implicit conversion
+            writeLineChar(asterisk);
+            writeLineUshort(asterisk); // implicit conversion
+            writeLineInt(asterisk);
+            writeLineUint(asterisk);
+            writeLineLong(asterisk);
+            writeLineUlong(asterisk);
+            writeLineFloat(asterisk);
+            writeLineDouble(asterisk);
+            writeLineDecimal(asterisk);
+            Console.WriteLine("space");
+            byte space = 32;
+            writeLineSbyte(space);
+            writeLineByte(space);
+            writeLineShort(space);
+            //writeLineChar(space); // no implicit conversion
+            writeLineUshort(space);
+            writeLineInt(space);
+            writeLineUint(space);
+            writeLineLong(space);
+            writeLineUlong(space);
+            writeLineFloat(space);
+            writeLineDouble(space);
+            writeLineDecimal(space);
+            Console.Write("Press <Enter>");
+            Console.ReadLine();
+        }
+
+        private static void writeLineByte(byte character)
+        {
+            Console.WriteLine((int)character);
+        }
+
+        private static void writeLineChar(char character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineDecimal(decimal character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineDouble(double character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineFloat(float character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineInt(int character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineLong(long character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineSbyte(byte character)
+        {
+            Console.WriteLine((int)character);
+        }
+
+        private static void writeLineShort(short character)
+        {
+            Console.WriteLine((int)character);
+        }
+
+        private static void writeLineUint(uint character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineUlong(ulong character)
+        {
+            Console.WriteLine(character);
+        }
+
+        private static void writeLineUshort(ushort character)
+        {
+            Console.WriteLine((int)character);
+        }
+    }
+}
---
+++ Managed/.NET/C#/Applications/Console/CharIntCompatibility/CharIntCompatibilityCSharpVB/Program.vb
@@ -0,0 +1,84 @@
+﻿Friend Class Program
+    ' Methods
+    Friend Shared Sub Main()
+        Console.WriteLine("asterisk")
+        Dim asterisk As Char = "*"c
+        'writeLineSbyte(asterisk) 'no implicit conversion
+        'writeLineByte(asterisk) 'no implicit conversion
+        'writeLineShort(asterisk) 'no implicit conversion        
+        writeLineChar(asterisk)
+        'writeLineUshort(asterisk) 'no implicit conversion
+        'writeLineInt(asterisk) 'no implicit conversion
+        'writeLineUint(asterisk) 'no implicit conversion
+        'writeLineLong(asterisk) 'no implicit conversion
+        'writeLineUlong(asterisk) 'no implicit conversion
+        'writeLineFloat(asterisk) 'no implicit conversion
+        'writeLineDouble(asterisk) 'no implicit conversion
+        'writeLineDecimal(asterisk) 'no implicit conversion
+        Console.WriteLine("space")
+        Dim space As Byte = 32
+        writeLineSbyte(space)
+        writeLineByte(space)
+        writeLineShort(space)
+        'writeLineChar(space) 'no implicit conversion
+        writeLineUshort(space)
+        writeLineInt(space)
+        writeLineUint(space)
+        writeLineLong(space)
+        writeLineUlong(space)
+        writeLineFloat(space)
+        writeLineDouble(space)
+        writeLineDecimal(space)
+        Console.Write("Press <Enter>")
+        Console.ReadLine()
+    End Sub
+
+    Private Shared Sub writeLineByte(ByVal character As Byte)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineChar(ByVal character As Char)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineDecimal(ByVal character As Decimal)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineDouble(ByVal character As Double)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineFloat(ByVal character As Single)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineInt(ByVal character As Integer)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineLong(ByVal character As Long)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineSbyte(ByVal character As Byte)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineShort(ByVal character As Short)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineUint(ByVal character As UInt32)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineUlong(ByVal character As UInt64)
+        Console.WriteLine(character)
+    End Sub
+
+    Private Shared Sub writeLineUshort(ByVal character As UInt16)
+        Console.WriteLine(character)
+    End Sub
+
+End Class
