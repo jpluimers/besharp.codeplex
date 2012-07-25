87111243b3421d9c4d0e752f3e98af9336b66682
jeroenp_cp <SND\jeroenp_cp>
2012-07-25 08:23:48 +0000

    All Library projects now in a Library solutuon; New project BeSharp.IO that contains the class FileHelper with methods to create files including their parent directories in one sweep.

---
+++ Managed/.NET/C#/Library/BeSharp.IO/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace BeSharp.IO
+{
+    public static class FileHelper
+    {
+
+        public static void CreateUnderlyingDirectory(string path)
+        {
+            string directoryPath = Path.GetDirectoryName(path);
+            Directory.CreateDirectory(directoryPath); // NOP if it exists, will create all parent directories if not
+        }
+
+        public static StreamWriter AppendTextOrCreateTextAndParentDirectories(string path)
+        {
+            StreamWriter result;
+            if (File.Exists(path))
+            {
+                result = File.AppendText(path);
+            }
+            else
+            {
+                CreateUnderlyingDirectory(path);
+                result = File.CreateText(path);
+            }
+            return result;
+        }
+
+        public static StreamWriter CreateOrOverwriteTextAndCreateParentDirectories(string path)
+        {
+            CreateUnderlyingDirectory(path);
+            StreamWriter result = new StreamWriter(path, false);
+            return result;
+        }
+    }
+}
