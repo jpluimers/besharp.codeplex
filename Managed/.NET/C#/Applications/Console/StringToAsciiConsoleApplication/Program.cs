0212d2abc87b524cc3ebad86c9aeeb9ab41f160e
jeroenp_cp <SND\jeroenp_cp>
2012-10-04 08:05:47 +0000

    StringToAsciiConsoleApplication using NormalizationForm.FormD and NormalizationForm.FormC; see http://wiert.me/2013/06/11/netc-from-unicode-to-ascii-yes-this-is-one-way-converting-diacritics-to-regular-ascii-characters and http://stackoverflow.com/questions/249087/how-do-i-remove-diacritics-accents-from-a-string-in-net

---
+++ Managed/.NET/C#/Applications/Console/StringToAsciiConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BeSharp;
+
+namespace StringToAsciiConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unicode = "áìôüç";
+            string ascii = unicode.ToAscii();
+            Console.WriteLine("Unicode\t{0}", unicode);
+            Console.WriteLine("ASCII\t{0}", ascii);
+        }
+    }
+}
