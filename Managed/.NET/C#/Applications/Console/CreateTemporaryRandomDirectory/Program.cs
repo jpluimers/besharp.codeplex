3142f315e00a65990820f4851bda6dce61f1325a
jeroenp_cp <SND\jeroenp_cp>
2012-10-18 15:17:26 +0000

    CreateTemporaryRandomDirectory

---
+++ Managed/.NET/C#/Applications/Console/CreateTemporaryRandomDirectory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BeSharp.IO;
+
+namespace CreateTemporaryRandomDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temporaryDirectory = DirectoryHelper.GetTemporaryDirectory();
+            Console.WriteLine("Temporary directory {0}", temporaryDirectory);
+        }
+    }
+}
