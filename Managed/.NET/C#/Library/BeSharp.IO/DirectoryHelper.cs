2521150d0362af99b0e38e0bef628bed82525b96
jeroenp_cp <SND\jeroenp_cp>
2012-10-18 15:17:26 +0000

    CreateTemporaryRandomDirectory

---
+++ Managed/.NET/C#/Library/BeSharp.IO/DirectoryHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace BeSharp.IO
+{
+    public class DirectoryHelper
+    {
+        public static string GetTemporaryDirectory()
+        {
+            do
+            {
+                try
+                {
+                    string tempPath = Path.GetTempPath();
+                    string randomFileName = Path.GetRandomFileName();
+                    string tempDirectory = Path.Combine(tempPath, randomFileName);
+                    Directory.CreateDirectory(tempDirectory);
+                    return tempDirectory;
+                }
+                catch (IOException /* ex */)
+                {
+                    // continue
+                }
+            } while (true);
+        }
+    }
+}
