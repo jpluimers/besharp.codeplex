2990b528fbcbf46fd42f1fd7bc36875703c31001
jeroenp_cp <SND\jeroenp_cp>
2013-01-07 13:04:15 +0000

    Delphi: RegExCRLF additional CStyleCRLFBuilder example.

--- Native/Delphi/Apps/RegEx/RegExCRLF/RegExCRLF.dpr
+++ Native/Delphi/Apps/RegEx/RegExCRLF/RegExCRLF.dpr
@@ -8,21 +8,41 @@ uses
   System.SysUtils,
   System.RegularExpressions;
 
+procedure RunRegEx(const CRLF: string);
 const
-  CStyleCRLF = '\r\n';
   MatchInput = sLineBreak;
   ReplaceInput = 'Foo Bar';
 var
   IsMatch: Boolean;
   Replacement: string;
+begin
+  Writeln('CRLF: "', CRLF, '"');
+  IsMatch := TRegEx.Match(MatchInput, CRLF).Success;
+  Writeln(IsMatch, ' for matching sLineBreak with ', CRLF);
+  Replacement := TRegEx.Replace(ReplaceInput, ' ', CRLF);
+  Writeln('Replacing spaces in "', ReplaceInput, '" with "', CRLF, '"');
+  Writeln('Results in "', Replacement, '"');
+end;
+
+const
+  CStyleCRLF = '\r\n';
+var
+  CStyleCRLFBuilder: TStringBuilder;
 
 begin
   try
-    IsMatch := TRegEx.Match(MatchInput, CStyleCRLF).Success;
-    Writeln(IsMatch, ' for matching sLineBreak with ', CStyleCRLF);
-    Replacement := TRegEx.Replace(ReplaceInput, ' ', CStyleCRLF);
-    Writeln('Replacing spaces in "', ReplaceInput, '" with "', CStyleCRLF,'"');
-    Writeln('Results in "', Replacement,'"');
+    Writeln('Delphi: CStyleCRLF expands to "', CStyleCRLF, '"');
+    RunRegEx(CStyleCRLF);
+    CStyleCRLFBuilder := TStringBuilder.Create();
+    try
+      CStyleCRLFBuilder.Append('\');
+      CStyleCRLFBuilder.Append('r');
+      CStyleCRLFBuilder.Append('\');
+      CStyleCRLFBuilder.Append('n');
+      RunRegEx(CStyleCRLFBuilder.ToString());
+    finally
+      CStyleCRLFBuilder.Free;
+    end;
   except
     on E: Exception do
       Writeln(E.ClassName, ': ', E.Message);
@@ -32,6 +52,12 @@ end.
 (*
 Output:
 
+Delphi: CStyleCRLF expands to "\r\n"
+CRLF: "\r\n"
+TRUE for matching sLineBreak with \r\n
+Replacing spaces in "Foo Bar" with "\r\n"
+Results in "Foo\r\nBar"
+CRLF: "\r\n"
 TRUE for matching sLineBreak with \r\n
 Replacing spaces in "Foo Bar" with "\r\n"
 Results in "Foo\r\nBar"
