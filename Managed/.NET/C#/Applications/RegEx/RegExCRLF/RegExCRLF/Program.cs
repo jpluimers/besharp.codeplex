5bce358fad4b5d591a4fe3df14877273505cab5a
jeroenp_cp <SND\jeroenp_cp>
2013-01-07 14:17:44 +0000

    Added more cases to the C# RegExCRLF example:                 RunRegEx("\x0D\x0A");                 RunRegEx("\x000D\x000A");                 RunRegEx(@"\x000D\x000A");                 RunRegEx(@"\u000D\u000A");                 RunRegEx(@"\x0D\x0A");                 RunRegEx(@"\u0D\u0A");

--- Managed/.NET/C#/Applications/RegEx/RegExCRLF/RegExCRLF/Program.cs
+++ Managed/.NET/C#/Applications/RegEx/RegExCRLF/RegExCRLF/Program.cs
@@ -36,6 +36,12 @@ namespace RegExCRLF
                 CRLFBuilder.Append(@"\");
                 CRLFBuilder.Append("n");
                 RunRegEx(CRLFBuilder.ToString());
+                RunRegEx("\x0D\x0A");
+                RunRegEx("\x000D\x000A");
+                RunRegEx(@"\x000D\x000A");
+                RunRegEx(@"\u000D\u000A");
+                RunRegEx(@"\x0D\x0A");
+                RunRegEx(@"\u0D\u0A");
             }
             catch (Exception ex)
             {
@@ -61,5 +67,35 @@ CRLF: '\r\n'
 True for matching Environment.NewLine with \r\n
 Replacing spaces in 'Foo Bar' with '\r\n'
 Results in 'Foo\r\nBar'
+CRLF: '
+'
+True for matching Environment.NewLine with
+
+Replacing spaces in 'Foo Bar' with '
+'
+Results in 'Foo
+Bar'
+CRLF: '
+'
+True for matching Environment.NewLine with
+
+Replacing spaces in 'Foo Bar' with '
+'
+Results in 'Foo
+Bar'
+CRLF: '\x000D\x000A'
+False for matching Environment.NewLine with \x000D\x000A
+Replacing spaces in 'Foo Bar' with '\x000D\x000A'
+Results in 'Foo\x000D\x000ABar'
+CRLF: '\u000D\u000A'
+True for matching Environment.NewLine with \u000D\u000A
+Replacing spaces in 'Foo Bar' with '\u000D\u000A'
+Results in 'Foo\u000D\u000ABar'
+CRLF: '\x0D\x0A'
+True for matching Environment.NewLine with \x0D\x0A
+Replacing spaces in 'Foo Bar' with '\x0D\x0A'
+Results in 'Foo\x0D\x0ABar'
+CRLF: '\u0D\u0A'
+System.ArgumentException: parsing "\u0D\u0A" - Insufficient hexadecimal digits.
 
 */
