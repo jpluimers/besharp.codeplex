23a97b538868f4f5bb1e61fae5a5ef26c951290b
jeroenp_cp <SND\jeroenp_cp>
2013-02-10 19:10:37 +0000

    BeSharp.Win32 assembly: added classes DiskFreeSpaceEx and DiskInfo so you can call DiskInfo.GetDiskFreeSpaceEx which calls Kernel32.GetDiskFreeSpaceEx. Refactored Kernel32Dll.cs: added "SetLastError = true" to all methods. Documented all methods.

---
+++ Managed/.NET/C#/Library/BeSharp.Win32/DiskFreeSpaceEx.cs
@@ -0,0 +1,18 @@
+﻿namespace BeSharp.Win32
+{
+    public class DiskFreeSpaceEx
+    {
+        public string DirectoryName { get; private set; }
+        public long FreeBytesAvailable { get; private set; }
+        public long TotalNumberOfBytes { get; private set; }
+        public long TotalNumberOfFreeBytes { get; private set; }
+
+        public DiskFreeSpaceEx(string directoryName, long freeBytesAvailable, long totalNumberOfBytes, long totalNumberOfFreeBytes)
+        {
+            DirectoryName = directoryName;
+            FreeBytesAvailable = freeBytesAvailable;
+            TotalNumberOfBytes = totalNumberOfBytes;
+            TotalNumberOfFreeBytes = totalNumberOfFreeBytes;
+        }
+    }
+}
