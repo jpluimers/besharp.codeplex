919e40afda90c87a1fb07f8a712067c1822a85c3
jeroenp_cp <SND\jeroenp_cp>
2013-02-10 19:10:37 +0000

    BeSharp.Win32 assembly: added classes DiskFreeSpaceEx and DiskInfo so you can call DiskInfo.GetDiskFreeSpaceEx which calls Kernel32.GetDiskFreeSpaceEx. Refactored Kernel32Dll.cs: added "SetLastError = true" to all methods. Documented all methods.

---
+++ Managed/.NET/C#/Library/BeSharp.Win32/DiskInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace BeSharp.Win32
+{
+    public class DiskInfo
+    {
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa364937
+        /// </summary>
+        /// <param name="directoryName">A directory on the disk.
+        /// If this parameter is NULL, the function uses the root of the current disk.
+        /// If this parameter is a UNC name, it must include a trailing backslash.</param>
+        /// <returns>DiskFreeSpaceEx on success, null on error.</returns>
+        public static DiskFreeSpaceEx GetDiskFreeSpaceEx(string directoryName)
+        {
+            long freeBytesAvailable;
+            long totalNumberOfBytes;
+            long totalNumberOfFreeBytes;
+
+            DiskFreeSpaceEx result;
+            if (Kernel32Dll.GetDiskFreeSpaceEx(directoryName, out freeBytesAvailable, out totalNumberOfBytes, out totalNumberOfFreeBytes))
+                result = new DiskFreeSpaceEx(directoryName, freeBytesAvailable, totalNumberOfBytes, totalNumberOfFreeBytes);
+            else
+                result = null;
+            return result;
+        }
+    }
+}
--- Managed/.NET/C#/Library/BeSharp.Win32/Kernel32Dll.cs
+++ Managed/.NET/C#/Library/BeSharp.Win32/Kernel32Dll.cs
@@ -7,19 +7,51 @@ namespace BeSharp.Win32
     {
         private const string kernel32_dll = "kernel32.dll";
 
-        [DllImport(kernel32_dll)]
+
+
+        /// <summary>
+        /// http://msdn.microsoft.com/library/windows/desktop/ms683152
+        /// Do not call this for PInvoke calls: http://stackoverflow.com/a/2445558/29290
+        /// Do not call this for handles obtained from GetModuleHandle.
+        /// </summary>
+        /// <param name="hModule">A handle to the loaded library module. The LoadLibrary, LoadLibraryEx, GetModuleHandle, or GetModuleHandleEx function returns this handle.</param>
+        /// <returns>Boolean indicating if the function succeeds.</returns>
+        [DllImport(kernel32_dll, SetLastError = true)]
         public static extern bool FreeLibrary(IntPtr hModule);
 
         /// <summary>
         /// Do not call FreeLibrary on a handle obtained from GetModuleHandle
-        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms683199(v=vs.85).aspx
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms683199
         /// </summary>
-        /// <param name="lpModuleName"></param>
-        /// <returns></returns>
-        [DllImport(kernel32_dll, CharSet = CharSet.Auto)]
+        /// <param name="lpModuleName">The name of the loaded module.</param>
+        /// <returns>Handle to the specified module. If the function fails, the return value is NULL.</returns>
+        [DllImport(kernel32_dll, SetLastError = true, CharSet = CharSet.Auto)]
         public static extern IntPtr GetModuleHandle(string lpModuleName);
 
-        [DllImport(kernel32_dll, CharSet = CharSet.Auto)]
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms684175
+        /// </summary>
+        /// <param name="lpModuleName">The name of the module.</param>
+        /// <returns>Handle to the specified module. If the function fails, the return value is NULL.</returns>
+        [DllImport(kernel32_dll, SetLastError = true, CharSet = CharSet.Auto)]
         public static extern IntPtr LoadLibrary(string lpModuleName);
+
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa364937
+        /// </summary>
+        /// <param name="lpDirectoryName">A directory on the disk.
+        /// If this parameter is NULL, the function uses the root of the current disk.
+        /// If this parameter is a UNC name, it must include a trailing backslash.</param>
+        /// <param name="lpFreeBytesAvailable">Total number of free bytes on a disk that are available to the user who is associated with the calling thread.</param>
+        /// <param name="lpTotalNumberOfBytes">Total number of bytes on a disk that are available to the user who is associated with the calling thread.</param>
+        /// <param name="lpTotalNumberOfFreeBytes">Total number of free bytes on a disk.</param>
+        /// <returns>Boolean indicating if the function succeeds.</returns>
+        [DllImport(kernel32_dll, SetLastError = true, CharSet = CharSet.Auto)]
+        public static extern bool GetDiskFreeSpaceEx(
+            string lpDirectoryName, 
+            out long lpFreeBytesAvailable, 
+            out long lpTotalNumberOfBytes, 
+            out long lpTotalNumberOfFreeBytes
+            );
     }
 }
