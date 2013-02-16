6884a9561f527659b20b577ba4090d3992254185
jeroenp_cp <SND\jeroenp_cp>
2013-02-16 15:51:34 +0000

    Added the kernel32.dll function GetVolumeInformation

--- Managed/.NET/C#/Library/BeSharp.Win32/DiskFreeSpaceEx.cs
+++ Managed/.NET/C#/Library/BeSharp.Win32/DiskFreeSpaceEx.cs
@@ -3,11 +3,11 @@
     public class DiskFreeSpaceEx
     {
         public string DirectoryName { get; private set; }
-        public long FreeBytesAvailable { get; private set; }
-        public long TotalNumberOfBytes { get; private set; }
-        public long TotalNumberOfFreeBytes { get; private set; }
+        public ulong FreeBytesAvailable { get; private set; }
+        public ulong TotalNumberOfBytes { get; private set; }
+        public ulong TotalNumberOfFreeBytes { get; private set; }
 
-        public DiskFreeSpaceEx(string directoryName, long freeBytesAvailable, long totalNumberOfBytes, long totalNumberOfFreeBytes)
+        public DiskFreeSpaceEx(string directoryName, ulong freeBytesAvailable, ulong totalNumberOfBytes, ulong totalNumberOfFreeBytes)
         {
             DirectoryName = directoryName;
             FreeBytesAvailable = freeBytesAvailable;
---
+++ Managed/.NET/C#/Library/BeSharp.Win32/VolumeInformation.cs
@@ -0,0 +1,23 @@
+﻿namespace BeSharp.Win32
+{
+    public class VolumeInformation
+    {
+        public string RootPathName { get; private set; }
+        public string VolumeName { get; private set; }
+        public uint VolumeSerialNumber { get; private set; }
+        public uint MaximumComponentLength { get; private set; }
+        public FileSystemFlags FileSystemFlags { get; private set; }
+        public string FileSystemName { get; private set; }
+
+        public VolumeInformation(string rootPathName, string volumeName, uint volumeSerialNumber, uint maximumComponentLength, FileSystemFlags fileSystemFlags, string fileSystemName)
+        {
+            RootPathName = rootPathName;
+            VolumeName = volumeName;
+            VolumeSerialNumber = volumeSerialNumber;
+            MaximumComponentLength = maximumComponentLength;
+            FileSystemFlags = fileSystemFlags;
+            FileSystemName = fileSystemName;
+        }
+
+    }
+}
