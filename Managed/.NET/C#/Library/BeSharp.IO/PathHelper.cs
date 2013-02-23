818f7f1c8969d5c5241f62b470c5a5c7707ef9d7
jeroenp_cp <SND\jeroenp_cp>
2013-02-23 20:58:32 +0000

    refactoring to make AssemblyHelper.DeploymentPath possible.

---
+++ Managed/.NET/C#/Library/BeSharp.IO/PathHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace BeSharp.IO
+{
+    public class PathHelper
+    {
+        public static string RemoveTrailingDirectorySeparators(string directory)
+        {
+            string result = directory.TrimEnd(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+            return result;
+        }
+    }
+}
