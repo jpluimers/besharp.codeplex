de1ba0205821cc423660367d4262f7a1b906630e
jeroenp_cp <SND\jeroenp_cp>
2013-02-24 16:23:53 +0000

    Added Pi as a test for the DecimalDoubleSingleTestProject

---
+++ Managed/.NET/C#/Tests/UnitTests/DecimalDoubleSingleTestProject/UnitTestPi.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DecimalDoubleSingleTestProject
+{
+    [TestClass]
+    public class UnitTestPi : UnitTestBase
+    {
+        /// <summary>
+        /// http://www.piday.org/million/
+        /// </summary>
+        private const decimal pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286m;
+        public UnitTestPi()
+            : base((float)Math.PI, Math.PI, pi)
+        {
+        }
+    }
+}
