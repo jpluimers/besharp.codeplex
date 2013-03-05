5c82987d276b4c85924e7c405eef9eac5c76a3a0
jeroenp_cp <SND\jeroenp_cp>
2013-03-05 14:27:19 +0000

    Some demos for an IGrouping blog article or SO question: IGroupingConsoleApplication

---
+++ Managed/.NET/C#/Applications/Console/IGroupingConsoleApplication/Fruit.cs
@@ -0,0 +1,28 @@
+﻿namespace IGroupingConsoleApplication
+{
+    public class Fruit
+    {
+        public string Type { get; private set; }
+        public string Variety { get; private set; }
+        public int  Quantity { get; private set; }
+        public decimal PricePerKilo { get; private set; }
+
+        public FruitVariety GetFruitVariety()
+        {
+            return new FruitVariety(Type, Variety);
+        }
+
+        public FruitVariety GetDistinctFruitVariety(string type, string variety)
+        {
+            return FruitVariety.GetDistinctFruitVariety(type, variety);
+        }
+
+        public Fruit(string type, string variety, int quantity, decimal pricePerKilo)
+        {
+            Type = type;
+            Variety = variety;
+            Quantity = quantity;
+            PricePerKilo = pricePerKilo;
+        }
+    }
+}
---
+++ Managed/.NET/C#/Applications/Console/IGroupingConsoleApplication/Grocery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace IGroupingConsoleApplication
+{
+    public class Grocery
+    {
+        public static IEnumerable<Fruit> BuyFrom()
+        {
+            const string Apple = "Apple";
+            const string Pear = "Pear";
+            const string Cherry = "Cherry";
+
+            Fruit[] result = new Fruit[]
+            {
+                new Fruit(Apple, "Belle de Boskoop", 12, 2.48m),
+                new Fruit(Apple, "Elstar", 7, 1.98m),
+                new Fruit(Apple, "Elstar", 9, 1.88m),
+                new Fruit(Apple, "Red Prince", 11, 2.98m),
+                new Fruit(Apple, "Santana", 5, 2.48m),
+                new Fruit(Pear, "Gieser Wildeman", 3, 3.48m),
+                new Fruit(Pear, "Verdi", 6, 3.98m),
+                new Fruit(Cherry, "Early Rivers", 30, 5.98m),
+                new Fruit(Cherry, "Morel", 25, 6.98m),
+            };
+            return result;
+        }
+    }
+}
